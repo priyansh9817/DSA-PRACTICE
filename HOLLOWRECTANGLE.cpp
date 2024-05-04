5c5c2e74ef40fd8a62bd1021afa5688c4c36af09
priyanshu9817 <priyanshu9817.s@gmail.com>
2024-05-04 11:20:34 +0530

    PAttern

--- HOLLOWRECTANGLE.cpp
+++ HOLLOWRECTANGLE.cpp
@@ -19,24 +19,4 @@ int main()
         cout << endl;
     }  
 }
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n, m;
-//     cout << "Enter the value of n: ";
-//     cin >> n;
-//     cout << "Enter the value of m: ";
-//     cin >> m;
-//     for (int i = 1; i <= n; i++) {
-//         for (int j = 1; j <= m; j++) {
-//             if (i == 1 || i == n || j == 1 || j == m) {
-//                 cout << " * ";
-//             }
-//             else {
-//                 cout << "   "; // Use spaces to maintain the rectangular shape
-//             }
-//         }
-//         cout << endl;
-//     }
-// }
+// NoTE: Space is matter 

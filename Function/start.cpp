cdc2e78a4257d18a24987937d39ddfc5f94ba04b
priyanshu9817 <priyanshu9817.s@gmail.com>
2024-05-05 12:55:05 +0530

    Function

---
+++ Function/start.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void doraemon(){
+   cout<<"Life end with you ";
+}
+int main (){
+   
+    char n;
+    cout<<"Enter the name :";
+    cin>>n;
+    doraemon();
+}

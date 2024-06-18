89b96c5a00c88c7c645709752b24d6d9a4ce4bf1
priyanshu9817 <priyanshu9817.s@gmail.com>
2024-06-18 20:53:15 +0530

    Practice

--- ARRAY/03_userInputOutput.cpp
+++ ARRAY/03_userInputOutput.cpp
@@ -13,21 +13,18 @@
 //         cout<<arr[i]<<" ";  
 //     }
 // }
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int n;
-    cout << "Enter the size of array: ";
-    cin >> n;
+    cout<<"Enter the size of array";
+    cin>>n;
     int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Enter the value of array: ";
-        cin >> arr[i];
+    for(int i=0; i<n;i++){
+        cout<<"Enter the value of array";
+        cin>>arr[i];
     }
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
 }
--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+void bubblesort(vector<int> &v){
+    int n=v.size();
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(v[j]>v[j+1]){
+                swap(v[j],v[j+1]);
+            }
+        }
+    }
+    return;
+}
 int main(){
     int n;
-    cout<<"Enter the value of n: ";
+    cout<<"enter the value of n ";
     cin>>n;
-    int arr[n];
-    for(int i=1;i<n-1;i++)
+
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    bubblesort(v);
+
+    for(int i =0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
 }
